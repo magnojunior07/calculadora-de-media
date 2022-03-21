66faacd070c34befc7529f282c5c0b9d6ad215e9
magnojunior07 <99290062+magnojunior07@users.noreply.github.com>
2022-03-21 11:52:12 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 function media() {
-    let b1Note1 = parseFloat(document.getElementById("b1Note1").value);
-    let b1Note2 = parseFloat(document.getElementById("b1Note2").value);
-    let b2Note1 = parseFloat(document.getElementById("b2Note1").value);
-    let b2Note2 = parseFloat(document.getElementById("b2Note2").value);
-    let b3Note1 = parseFloat(document.getElementById("b3Note1").value);
-    let b3Note2 = parseFloat(document.getElementById("b3Note2").value);
-    let b4Note1 = parseFloat(document.getElementById("b4Note1").value);
-    let b4Note2 = parseFloat(document.getElementById("b4Note2").value);
-
-    let notaSomada = b1Note1 + b1Note2 + b2Note1 + b2Note2 + b3Note1 + b3Note2 + b4Note1 + b4Note2;
+    let notaSomada = parseFloat(document.getElementById("b1Note1 + b1Note2 + b2Note1 + b2Note2 + b3Note1 + b3Note2 + b4Note1 + b4Note2").value);
     let media = notaSomada/8;
     let notaFixada = media.toFixed(1);
 
