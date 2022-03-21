83dac7107fda01a4ab2f8823d9de8a37aa6ecf52
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-03-21 11:08:59 -0400

    detecta se o usario é aprovado.

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const res_container = document.getElementById("res-container");
 let calculate_btn = document.getElementById("calculate-btn");
 let inputs = document.querySelectorAll("input");
 let anualNotes = [];
-let averageNote = 0;
 let res = 0;
+let isAprove = false;
+let averageUserNote = 0;
+
 
 calculate_btn.addEventListener("click", () => {
     anualNotes = [];
@@ -21,9 +23,13 @@ calculate_btn.addEventListener("click", () => {
 
     });
 
-    calculateAverage();
+    averageUserNote = calculateAverage();
 
-    res = `<h2> Sua média anual: ${averageNote.toFixed(2)} </h2?`;
+    res = `
+        <h2> Sua média anual: ${averageUserNote} </h2>
+        <br>
+        <p> Voçe esta ${isAprove ? "aprovado" : "reprovado"}</p>
+        `;
     res_container.innerHTML = res;
 });
 
@@ -31,6 +37,8 @@ calculate_btn.addEventListener("click", () => {
 function calculateAverage() {
     
     let sumOfAllNotes = 0;
+    let averageNote = 0;
+
     for (let i = 0; i < anualNotes.length; i++) {
 
         sumOfAllNotes += anualNotes[i];
@@ -38,5 +46,15 @@ function calculateAverage() {
     };
     averageNote = sumOfAllNotes / anualNotes.length;
 
+    
 
+    if (averageNote >= 7.0) {
+        isAprove = true
+    }
+
+    if (averageNote == NaN) {
+        return 0.0
+    } else {
+        return averageNote.toFixed(2)
+    }
 }
